a42efb3e747dce1868845d24229cca11963fb5ca
정관영 <kwanGDss@users.noreply.github.com>
2025-09-26 14:55:01 +0900

    first commit

---
+++ src/main/java/com/smhrd/ncp_project/NcpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.ncp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smhrd/ncp_project/NcpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smhrd.ncp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
